03c1e588e53e3396ac8b8674ec19c93aba2a6200
Suzuka-Samidare <saberlily031@gmail.com>
2026-02-12 17:58:39 +0900

    feat(util): タイルの範囲参照メソッドをユーティリティに移動 & 距離のクランプ処理を撤廃し、使いまわし出来るように改善

--- Assets/Scripts/UI/TestButton.cs
+++ Assets/Scripts/UI/TestButton.cs
@@ -8,16 +8,24 @@ public class TestButton : MonoBehaviour
     public BaseUnitData unitData;
 
     private MapManager _mapManager;
+    private TileManager _tileManager;
 
     void Start()
     {
         _mapManager = MapManager.Instance;
+        _tileManager = TileManager.Instance;
     }
 
     public void Onclick()
     {
-        _mapManager.RevealManhattanRange(4, 3, 3, (y, x) => {
-            _mapManager.playerMapData[y, x].SpawnUnitDelayed(unitData);
+        int centerY = _tileManager.selectedTileController.gridPosY;
+        int centerX = _tileManager.selectedTileController.gridPosX;
+
+        TileRangeUtil.ForEachManhattanRange(centerY, centerX, 3, (y, x) => {
+            if (y >= 0 && y < _mapManager.mapHeight && x >= 0 && x < _mapManager.mapWidth)
+            {
+                _mapManager.playerMapData[y, x].SpawnUnitDelayed(unitData);
+            }
         });
     }
 }
---
+++ Assets/Scripts/Utility/TileRangeUtil.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+public static class TileRangeUtil
+{
+    public static void ForEachManhattanRange(int centerY, int centerX, int range, Action<int, int> action)
+    {
+        for (int y = centerY - range; y <= centerY + range; y++)
+        {
+            // Y軸方向の距離を計算
+            int distY = Mathf.Abs(y - centerY);
+            // 残りの移動可能距離（range - distY）がX軸に割ける最大幅になる
+            int xRemaining = range - distY;
+
+            for (int x = centerX - xRemaining; x <= centerX + xRemaining; x++)
+            {
+                // 座標を引数として設定して外部からDoSomething
+                action(y, x);
+            }
+        }
+    }
+}
